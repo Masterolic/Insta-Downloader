08fc42dc0393ca5c73cf6dd17bec245504ff0fda
Masterolic <rockingmallu8@gmail.com>
2023-07-08 10:00:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,2 +1,10 @@
 from pyrogram import filters, Client 
 import bs4, requests
+
+@Mbot.on_message(filters.command("start") & filters.incoming)
+async def start(Mbot, message):
+          await message.reply(f"Hello 👋👋 {message.from_user.mention()}\n I am A Simple Fastest  Instagram Downloader Bot Currently Supports Reels and Post")
+@Mbot.on_message(filters.command("help") & filters.incoming)
+async def start(Mbot, message):
+          await message.reply(f"Help Guide For  {message.from_user.mention()}\n I am A Simple Fastest  Instagram Downloader Bot Currently Supports Reels and Post \n reel: `https://www.instagram.com/reel/CuCTtORJbDj/?igshid=MzRlODBiNWFlZA==`\n post: `https://www.instagram.com/reel/CZqWDGODoov/?igshid=MzRlODBiNWFlZA==`")
+
