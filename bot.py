0d65f689cab8e089d58add3a3caae8f73ec29cbb
Masterolic <rockingmallu8@gmail.com>
2023-07-08 09:52:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,37 +14,11 @@ Mbot=Client(name="instabot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
+            plugins=dict(root="plugins"),
             sleep_threshold=22)
 @Mbot.on_message(filters.command("start") & filters.incoming)
 async def start(Mbot, message):
           await message.reply(f"Hello 👋👋 {message.from_user.mention()}\n I am A Simple Fastest  Instagram Downloader Bot Currently Supports Reels and Post")
-@Mbot.on_message(filters.regex(r'https?://.*instagram[^\s]+'), group=1)
-async def link_handler(Mbot, message):
-    link = message.matches[0].group(0)
-    try:
-        m = await message.reply_text("⏳")
-        url= link.replace("instagram.com","ddinstagram.com")
-        await message.reply_video(url)
-    except Exception as e:
-        try:
-            if "/reel/" in url:
-               getdata = requests.get(url).text
-               soup = bs4.BeautifulSoup(getdata, 'html.parser')
-               meta_tag = soup.find('meta', attrs={'property': 'og:video'})
-               content_value = meta_tag['content']
-               await message.reply_video(f"https://ddinstagram.com{content_value}")
-            elif "/p/" in url:
-                 getdata = requests.get(url).text
-                 soup = bs4.BeautifulSoup(getdata, 'html.parser')
-                 meta_tag = soup.find('meta', attrs={'property': 'og:image'})
-                 content_value = meta_tag['content']
-                 await message.reply_photo(f"https://ddinstagram.com{content_value}")
-        except Exception as e:
-            await message.reply_text(f"https://ddinstagram.com{content_value}")
-            if LOG_GROUP:
-               await Mbot.send_message(LOG_GROUP,f"Instagram {e} {content_value}")
-            ##optinal 
-            await message.reply(f"400: Sorry, Unable To Find It  try another or report it  to @masterolic or support chat @spotify_supportbot 🤖  ")
 if __name__ == '__main__':
     print (" Insta-DL Bot started  running...")
 
