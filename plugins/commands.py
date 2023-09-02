5191155332f5b51e9b9b673e7bceff5cbf0d72d0
Masterolic <rockingmallu8@gmail.com>
2023-09-02 17:34:01 +0530

    Update commands.py
    Fixed start not responding

--- plugins/commands.py
+++ plugins/commands.py
@@ -5,5 +5,5 @@ import bs4, requests
 async def start(Mbot, message):
           await message.reply(f"Hello 👋👋 {message.from_user.mention()}\n I am A Simple Fastest  Instagram Downloader Bot Currently Supports Reels and Post")
 @Mbot.on_message(filters.command("help") & filters.incoming)
-async def start(Mbot, message):
+async def help(Mbot, message):
           await message.reply("This is user friendly bot so you can simple send your Instagram reel and post links here:) \n eg: `https://www.instagram.com/reel/CZqWDGODoov/?igshid=MzRlODBiNWFlZA==`\n `post:` `https://www.instagram.com/reel/CuCTtORJbDj/?igshid=MzRlODBiNWFlZA==`")
