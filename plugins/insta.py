3b46bc62ef1b19bed5b1b6d991b7c24fa209ea87
Masterolic <rockingmallu8@gmail.com>
2023-09-07 21:37:03 +0530

    Update insta.py

--- plugins/insta.py
+++ plugins/insta.py
@@ -1,13 +1,15 @@
 from pyrogram import filters, Client as Mbot
 import bs4, requests
-from bot import LOG_GROUP
+from bot import LOG_GROUP,DUMP_GROUP
 @Mbot.on_message(filters.regex(r'https?://.*instagram[^\s]+'), group=1)
 async def link_handler(Mbot, message):
     link = message.matches[0].group(0)
     try:
         m = await message.reply_text("⏳")
         url= link.replace("instagram.com","ddinstagram.com")
-        await message.reply_video(url)
+        dump_file=await message.reply_video(url)
+        if 'dump_file' in locals():
+           await dump_file.forward(DUMP_GROUP)
     except Exception as e:
         try:
             if "/reel/" in url:
@@ -15,13 +17,13 @@ async def link_handler(Mbot, message):
                soup = bs4.BeautifulSoup(getdata, 'html.parser')
                meta_tag = soup.find('meta', attrs={'property': 'og:video'})
                content_value = meta_tag['content']
-               await message.reply_video(f"https://ddinstagram.com{content_value}")
+               dump_file=await message.reply_video(f"https://ddinstagram.com{content_value}")
             elif "/p/" in url:
                  getdata = requests.get(url).text
                  soup = bs4.BeautifulSoup(getdata, 'html.parser')
                  meta_tag = soup.find('meta', attrs={'property': 'og:image'})
                  content_value = meta_tag['content']
-                 await message.reply_photo(f"https://ddinstagram.com{content_value}")
+                 dump_file=await message.reply_photo(f"https://ddinstagram.com{content_value}")
         except Exception as e:
             await message.reply_text(f"https://ddinstagram.com{content_value}")
             if LOG_GROUP:
@@ -29,3 +31,7 @@ async def link_handler(Mbot, message):
             ##optinal 
             await message.reply(f"400: Sorry, Unable To Find It  try another or report it  to @masterolic or support chat @spotify_supportbot 🤖  ")
 
+        finally:
+            if 'dump_file' in locals():
+               await dump_file.forward(DUMP_GROUP)
+                
