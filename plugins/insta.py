1d4b71b973bd001677a3cf52ba33e4c57dac59a1
Masterolic <rockingmallu8@gmail.com>
2023-09-20 21:38:29 +0530

    Update insta.py
    Fixed issue curl webpage failed

--- plugins/insta.py
+++ plugins/insta.py
@@ -1,5 +1,6 @@
 from pyrogram import filters, Client as Mbot
 import bs4, requests
+import wget,os
 from bot import LOG_GROUP,DUMP_GROUP
 @Mbot.on_message(filters.regex(r'https?://.*instagram[^\s]+') & filters.incoming, group=1)
 async def link_handler(Mbot, message):
@@ -22,13 +23,15 @@ async def link_handler(Mbot, message):
                soup = bs4.BeautifulSoup(getdata, 'html.parser')
                meta_tag = soup.find('meta', attrs={'property': 'og:video'})
                content_value = meta_tag['content']
-               dump_file=await message.reply_video(f"https://ddinstagram.com{content_value}")
+               downfile=wget.download(f"https://ddinstagram.com{content_value}")
+               dump_file=await message.reply_video(downfile) 
             elif "/p/" in url:
                  getdata = requests.get(url).text
                  soup = bs4.BeautifulSoup(getdata, 'html.parser')
                  meta_tag = soup.find('meta', attrs={'property': 'og:image'})
                  content_value = meta_tag['content']
-                 dump_file=await message.reply_photo(f"https://ddinstagram.com{content_value}")
+                 downfile=wget.download(f"https://ddinstagram.com{content_value}")
+                 dump_file=await message.reply_photo(downfile)
         except Exception as e:
             await message.reply_text(f"https://ddinstagram.com{content_value}")
             if LOG_GROUP:
@@ -41,4 +44,5 @@ async def link_handler(Mbot, message):
                if DUMP_GROUP:
                   await dump_file.forward(DUMP_GROUP)
             await m.delete()
+            os.remove(downfile)
                 
