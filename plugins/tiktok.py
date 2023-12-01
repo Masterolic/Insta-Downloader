afaa1fac806e68339d895cf9f5c7596b5d55be15
Masterolic <rockingmallu8@gmail.com>
2023-12-01 21:43:52 +0530

    TypeError

--- plugins/tiktok.py
+++ plugins/tiktok.py
@@ -13,14 +13,14 @@ async def link_handler(Mbot, message):
            return await message.reply("Oops Invalid TikTok video url. Please try again :) ")
         if get_api.get('play_url'):
            try:
-              dump_file await message.reply_video(get_api['play_url'])
+              dump_file = await message.reply_video(get_api['play_url'])
            except KeyError:
                return await message.reply("Invalid TikTok video url. Please try again.")
            except Exception:
                snd_msg=await message.reply(get_api['play_url'])
                await asyncio.sleep(1)
                try:
-                  dump_file=await message.reply_video(get_api['play_url'])
+                  dump_file = await message.reply_video(get_api['play_url'])
                   await snd_msg.delete()
                except Exception:
                    pass
