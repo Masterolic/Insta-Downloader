565e7f967e0135e687a40cccd3e996e63e5a2d08
Masterolic <rockingmallu8@gmail.com>
2023-12-06 15:05:21 +0530

    Update insta.py

--- plugins/insta.py
+++ plugins/insta.py
@@ -42,7 +42,7 @@ async def link_handler(Mbot, message):
                if not meta_tag:
                   ddinsta=False
                   meta_tag = requests.post("https://saveig.app/api/ajaxSearch", data={"q": link, "t": "media", "lang": "en"}, headers=headers)
-                  print(meta_tag)
+             #     print(meta_tag)
                   if meta_tag.ok:
                      res=meta_tag.json()
                      print(res)
