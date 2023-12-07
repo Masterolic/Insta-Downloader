c694767b19db5ade9daa519938bdc1021692a19b
Masterolic <rockingmallu8@gmail.com>
2023-12-07 21:22:09 +0530

    Update YouTube.py

--- plugins/YouTube.py
+++ plugins/YouTube.py
@@ -82,9 +82,9 @@ async def _(Mbot,message):
             fileLink = await  ytdl_down(randomdir,id, message.from_user.id)
             print("down completely")
             thumnail = await thumb_down(id[0])
-            await message.reply(fileLink)
-            await message.reply_audio(fileLink)
-            AForCopy = await message.reply_audio(fileLink,caption=f"[{id[3]}](https://youtu.be/{id[0]}) - {id[2]}",title=id[3].replace("_"," "),performer=id[2],thumb=thumnail,duration=id[4])
+          #  await message.reply(fileLink)
+        #    await message.reply_audio(fileLink)
+            AForCopy = await message.reply_audio(fileLink,caption=f"[{id[3]}](https://youtu.be/{id[0]}) - {id[2]} Thank you for using - @InstaReelsdownbot",title=id[3].replace("_"," "),performer=id[2],thumb=thumnail,duration=id[4])
             if LOG_GROUP:
                 await PForCopy.copy(LOG_GROUP)
                 await AForCopy.copy(LOG_GROUP)
