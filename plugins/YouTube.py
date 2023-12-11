13f7788b800411f7411c135a029079287d27aee3
Masterolic <rockingmallu8@gmail.com>
2023-12-11 09:30:48 +0530

    Update YouTube.py

--- plugins/YouTube.py
+++ plugins/YouTube.py
@@ -118,6 +118,7 @@ async def _(Mbot,message):
             if LOG_GROUP:
                await Mbot.send_message(LOG_GROUP,f"YouTube Shorts {e} {link}")
                await message.reply(f"400: Sorry, Unable To Find It  try another or report it  to @masterolic or support chat @spotify_supportbot 🤖  ")
+               print(traceback.format_exc())
                await Mbot.send_message(LOG_GROUP, traceback.format_exc())
                
         return await message.reply("Check out @spotify_downloa_bot(music)  @spotifynewss(Channel) \n Please Support Us By /donate To Maintain This Project")
